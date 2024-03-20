08723bd7b759a5da774e57a98772b138cb3f92d9
gokula152 <151764082+gokula152@users.noreply.github.com>
2024-03-20 23:23:37 +0100

    Exception handling in datareader file

--- ZooExpenseTracker/ZooExpenseTracker.Lib/Services/DataFileReader.cs
+++ ZooExpenseTracker/ZooExpenseTracker.Lib/Services/DataFileReader.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -14,67 +14,86 @@ namespace ZooExpenseTracker.Lib.Services
     public class DataFileReader : IDataFileReader
     {
         public List<Animal> ReadAnimals(IFormFile animalsFile,  List<Animal>  animals)
-        {            
-            using (StreamReader reader = new StreamReader(animalsFile.OpenReadStream()))
+        {
+            try
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                using (StreamReader reader = new StreamReader(animalsFile.OpenReadStream()))
                 {
-                    var parts = line.Split(';');
-                    if (parts.Length == 4)
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
                     {
-                        string name = parts[0].Trim();
-                        decimal foodRate = Convert.ToDecimal(parts[1].Trim(), CultureInfo.InvariantCulture);
-                        string foodPreference = parts[2].Trim();
-                        decimal meatPercentage = !string.IsNullOrEmpty(parts[3].Trim()) ?
-                                                 Convert.ToDecimal(parts[3].Trim().Substring(0, parts[3].Length - 1), CultureInfo.InvariantCulture) / 100 : 0;
-
-                        foreach(var animal in animals.Where(animal => animal.AnimalType == name))
+                        var parts = line.Split(';');
+                        if (parts.Length == 4)
                         {
-                            animal.FoodRate = foodRate;
-                            animal.FoodPreference = foodPreference;
-                            animal.MeatPercentage = meatPercentage;
-                        }                    
-                        
+                            string name = parts[0].Trim();
+                            decimal foodRate = Convert.ToDecimal(parts[1].Trim(), CultureInfo.InvariantCulture);
+                            string foodPreference = parts[2].Trim();
+                            decimal meatPercentage = !string.IsNullOrEmpty(parts[3].Trim()) ?
+                                                     Convert.ToDecimal(parts[3].Trim().Substring(0, parts[3].Length - 1), CultureInfo.InvariantCulture) / 100 : 0;
+
+                            foreach (var animal in animals.Where(animal => animal.AnimalType == name))
+                            {
+                                animal.FoodRate = foodRate;
+                                animal.FoodPreference = foodPreference;
+                                animal.MeatPercentage = meatPercentage;
+                            }
+
+                        }
                     }
                 }
+                return animals;
             }
-            return animals;
+            catch(Exception ex)
+            {
+                return null;
+            }
+            
         }
 
         public FoodPrice ReadFoodPrices(IFormFile pricesFile)
         {
-            var foodPrices = new FoodPrice();
-
-            using (StreamReader reader = new StreamReader(pricesFile.OpenReadStream()))
+            try
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                var foodPrices = new FoodPrice();
+
+                using (StreamReader reader = new StreamReader(pricesFile.OpenReadStream()))
                 {
-                    var parts = line.Split('=');
-                    if (parts.Length == 2)
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
                     {
-                        string foodType = parts[0].Trim();
-                        var price = Convert.ToDecimal(parts[1].Trim(), CultureInfo.InvariantCulture);
-                        if(foodType == "Meat")
-                            foodPrices.MeatPricePerKg = price;
-                        if (foodType == "Fruit")
-                            foodPrices.FruitPricePerKg = price;
+                        var parts = line.Split('=');
+                        if (parts.Length == 2)
+                        {
+                            string foodType = parts[0].Trim();
+                            var price = Convert.ToDecimal(parts[1].Trim(), CultureInfo.InvariantCulture);
+                            if (foodType == "Meat")
+                                foodPrices.MeatPricePerKg = price;
+                            if (foodType == "Fruit")
+                                foodPrices.FruitPricePerKg = price;
+                        }
                     }
                 }
-            }
 
-            return foodPrices;
+                return foodPrices;
+
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+            
         }
 
         public List<Animal> ReadZoo(IFormFile zooFiles)
         {
+            try
+            {
                 List<Animal> animals = new List<Animal>();
 
-                
+
                 XmlDocument doc = new XmlDocument();
                 doc.Load(zooFiles.OpenReadStream());
-                
+
                 foreach (XmlNode animalNode in doc.DocumentElement.ChildNodes)
                 {
                     string animalType = animalNode.Name;
@@ -89,6 +108,13 @@ namespace ZooExpenseTracker.Lib.Services
                 }
 
                 return animals;
+
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+                
             }
         }
 }
